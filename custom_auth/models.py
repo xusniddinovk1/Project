f1b429f07200cfafe363e1c8ce06f5e2dbadef81
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-06-14 16:45:44 +0500

    create custom user

--- custom_auth/models.py
+++ custom_auth/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, phone_number, password=None, **extra_fields):
+        if not phone_number:
+            raise ValueError('Phone number is must be set')
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
