74cd30be0da71165d920221bdb4169ddc56c4208
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-06-18 15:04:47 +0500

    create custom user

--- custom_auth/models.py
+++ custom_auth/models.py
@@ -10,3 +10,27 @@ class CustomUserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
+
+    def create_superuser(self, phone_number, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if not extra_fields.get('is_staff'):
+            raise ValueError('The user must be staff')
+        if not extra_fields.get('is_superuser'):
+            raise ValueError('The user must be superuser')
+        return self.create_user(phone_number, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    phone_number = models.CharField(max_length=13, unique=True, null=False)
+    data_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.phone_number
