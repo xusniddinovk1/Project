19669cce4a37e446185e1c5ffa67287b6b6e2a1f
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-06-21 17:35:57 +0500

    update models

--- core/models.py
+++ core/models.py
@@ -1,11 +1,18 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.text import slugify
 
 
 class AboutUs(models.Model):
     title = models.CharField(max_lenght=100)
+    slug = models.SlugField(unique=True, blank=True)
     description = models.TextField()
     image = models.ImageField(upload_to='aboutUs/')
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
@@ -13,8 +20,14 @@ class AboutUs(models.Model):
 
 class News(models.Model):
     title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, blank=True)
     content = models.TextField()
     image = models.ImageField(upload_to='news/')
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
@@ -22,9 +35,15 @@ class News(models.Model):
 
 class Course(models.Model):
     name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, blank=True)
     description = models.TextField()
     image = models.ImageField(upload_to='course')
     teacher = models.ForeignKey(User, on_delete=models.SET_NULL)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -32,12 +51,18 @@ class Course(models.Model):
 
 class Lesson(models.Model):
     title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, blank=True)
     description = models.TextField()
     image = models.ImageField(upload_to='lessons/')
     description1 = models.TextField()
     image1 = models.ImageField(upload_to='lessons/')
     description2 = models.TextField()
     image2 = models.ImageField(upload_to='lessons/')
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
