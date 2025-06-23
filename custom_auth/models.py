866ac0b8ccdfb588b2184de2d7251c4fc4a9b8e1
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-06-23 18:24:02 +0500

    udate custom user

--- custom_auth/models.py
+++ custom_auth/models.py
@@ -3,25 +3,30 @@ from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, Permis
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, phone_number, password=None, **extra_fields):
+    def create_user(self, username, phone_number, password=None, **extra_fields):
         if not phone_number:
-            raise ValueError('Phone number is must be set')
-        user = self.model(phone_number=phone_number, **extra_fields)
+            raise ValueError('Phone number must be set')
+        if not username:
+            raise ValueError('Username must be set')
+        user = self.model(username=username, phone_number=phone_number, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, phone_number, password=None, **extra_fields):
+    def create_superuser(self, username, phone_number, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        if not extra_fields.get('is_staff'):
-            raise ValueError('The user must be staff')
-        if not extra_fields.get('is_superuser'):
-            raise ValueError('The user must be superuser')
-        return self.create_user(phone_number, password, **extra_fields)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(username, phone_number, password, **extra_fields)
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=50, null=False, blank=False)
     phone_number = models.CharField(max_length=13, unique=True, null=False)
     date_joined = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
@@ -30,7 +35,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'phone_number'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username']
 
     def __str__(self):
-        return self.phone_number
+        return self.username
