1fd0a2efe10a458aaa257f842f9ef3b98a7408d9
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-06-29 18:56:16 +0500

    initial commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from accounts.views import *
 
-
 urlpatterns = [
-    path('register', register_page, name='register_page'),
     path('', index_page, name='index_page'),
+
+    path('register', register_page, name='register_page'),
     path('login/', login_page, name='login'),
     path('logout/', logout_page, name='logout'),
 ]
