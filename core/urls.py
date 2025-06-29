a9a820dc430677739497a3036d0b3cdadc807bd8
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-06-29 18:06:48 +0500

    initial commit

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,4 @@ from core.views import *
 
 urlpatterns = [
     path('', home_page, name='home_page'),
-    path('courses/<int:id>', course_detail, name='course_detail'),
 ]
--- core/views.py
+++ core/views.py
@@ -5,29 +5,14 @@ from core.models import AboutUs, News, Lesson, Course, MainPhoto, Teacher
 def home_page(request):
     about_us = AboutUs.objects.all()
     news_item = News.objects.order_by('-created_at')[:6]
-    courses = Course.objects.all()[:6]
-    lessons = Lesson.objects.all()[:6]
-    teachers = Teacher.objects.all()[:8]
-    main_photo = MainPhoto.objects.last()
+    courses = Course.objects.count()
+    lessons = Lesson.objects.count()
+    teachers = Teacher.objects.count()
 
     ctx = {
-        'about_us': about_us,
-        'news_items': news_item,
-        'courses': courses,
-        'lessons': lessons,
-        'teachers': teachers,
-        'main_photo': main_photo
+        'courses_count': courses,
+        'teachers_count': teachers,
+        'lessons_count': lessons,
     }
+
     return render(request, 'core/index.html', ctx)
-
-
-def course_detail(request, id):
-    course = Course.objects.get(pk=id)
-
-    related_courses = Course.objects.order_by('-created_at')[:6]
-
-    ctx = {
-        'course': course,
-        'related_courses': related_courses
-    }
-    return render(request, 'core/course/course_detail.html', ctx)
