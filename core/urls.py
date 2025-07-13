e55d9e032b6b6e06edb8e3efcdd7d23cab8edca3
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-13 22:21:46 +0500

    initial commit

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from core.views import *
 
 urlpatterns = [
-    path('', home_page, name='home_page'),
+    path('', home_page, name='home'),
     path('success/', success_view, name='success'),
     path('news/', news_page, name='news_page'),
     path('news/<slug:slug>', news_detail_page, name='news_detail_page'),
